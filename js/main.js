999d31c773802f1e1a31292f4b7c0b8f1b3ba5a2
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-08-12 00:15:29 -0700

    Refactor to product component.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,72 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Vue Mastery',
-    product: 'Socks',
-    selectedVariant: 0,
-    details: ['80% cotton', '20% polyester', 'gender-neutral'],
-    sizes: ['small', 'medium', 'large'],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: 'green',
-        variantImage: 'src/img/vmSocks-green-onWhite.jpg',
-        variantQuantity: 10
-      },
-      {
-        variantId: 2235,
-        variantColor: 'blue',
-        variantImage: 'src/img/vmSocks-blue-onWhite.jpg',
-        variantQuantity: 0
-      }
-    ],
-    cart: 0
+Vue.component('product', {
+  template: `
+    <div class="product">
+      <div class="product-image">
+        <img :src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p>{{ inventory }} left</p>
+        <p v-if="inventory > 10">In Stock</p>
+        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+        <p v-else>Out of Stock</p>
+
+        <h3>Details</h3>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <h3>Sizes</h3>
+        <ul>
+          <li v-for="size in sizes">{{ size }}</li>
+        </ul>
+
+        <h3>Colors</h3>
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.variantId"
+          class="color-box"
+          :style="{ backgroundColor: variant.variantColor }"
+          @mouseover="updateProduct(index)"
+        >
+        </div>
+
+        <button
+          v-on:click="addToCart"
+          :disabled="inventory < 1"
+          :class="{ disabledButton: inventory < 1 }"
+        >
+          Add to Cart
+        </button>
+        <div class="cart">
+          <p>Cart ({{ cart }})</p>
+        </div>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      brand: 'Vue Mastery',
+      product: 'Socks',
+      selectedVariant: 0,
+      details: ['80% cotton', '20% polyester', 'gender-neutral'],
+      sizes: ['small', 'medium', 'large'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: 'src/img/vmSocks-green-onWhite.jpg',
+          variantQuantity: 10
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: 'src/img/vmSocks-blue-onWhite.jpg',
+          variantQuantity: 0
+        }
+      ],
+      cart: 0
+    }
   },
   methods: {
     addToCart: function() {
@@ -42,4 +88,8 @@ var app = new Vue({
       return this.variants[this.selectedVariant].variantQuantity;
     }
   }
+});
+
+var app = new Vue({
+  el: '#app'
 });
